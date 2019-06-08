74dac7dbe3a896a2935d31eb408cbcce3a4b94c3
@maxgfr <maxgolfier@gmail.com>
2019-06-08 12:25:11 +0200

    push

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,24 +10,12 @@ import {
   View,
 } from 'react-native';
 
-import { MonoText } from '../components/StyledText';
-
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
 
         <View style={styles.getStartedContainer}>
           <DevelopmentModeNotice />
@@ -36,7 +24,6 @@ export default function HomeScreen() {
 
           <View
             style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
           </View>
 
           <Text style={styles.getStartedText}>
@@ -60,9 +47,6 @@ export default function HomeScreen() {
 
         <View
           style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
-          </MonoText>
         </View>
       </View>
     </View>
