a0440b28b4bb9ae730c73cdfcb178129b151fb3d
@maxgfr <maxgolfier@gmail.com>
2019-06-20 11:59:58 +0200

    push

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -8,39 +8,91 @@ import {
   Left,
   Right,
   Button,
-  Icon
+  Icon,
+  Text
 } from "native-base";
+import {
+  ActivityIndicator,
+  View
+} from "react-native";
 
 export default class ChatScreen extends React.Component {
 
   state = {
     messages: [],
+    session_id: '',
+    context: {},
+    id_message: 1,
+    is_waiting: true
   }
 
   static navigationOptions = {
     header: null,
   };
 
-  componentWillMount() {
+  async componentWillMount() {
+    const response_serv = await fetch('https://insurassistant-anxious-hyena.eu-gb.mybluemix.net/create-session', {
+        method: 'GET'
+    });
+    var result_serv = await response_serv.json();
+    console.log(result_serv);
     this.setState({
       messages: [
         {
-          _id: '1',
-          text: 'Salut',
+          _id: this.state.id_message,
+          text: 'Hi ! How can I help you ?',
           createdAt: new Date(),
           user: {
-            _id: '2',
+            _id: 'watson_id',
             name: 'Watson',
             avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/00/IBM_Watson_Logo_2017.png/220px-IBM_Watson_Logo_2017.png',
           },
         },
       ],
+      session_id: result_serv.data.session_id,
+      is_waiting: false
     })
   }
 
-  onSend(messages = []) {
+  async onSend(messages = []) {
+    var message = messages[0].text;
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
+      is_waiting: true
+    }))
+    var data = {
+    	message: message,
+    	context: this.state.context,
+    	session_id: this.state.session_id
+    }
+    console.log(data)
+    const response_serv = await fetch('https://insurassistant-anxious-hyena.eu-gb.mybluemix.net/send-message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    });
+    var result_serv = await response_serv.json();
+    console.log(result_serv)
+    var id_msg = this.state.id_message + 1;
+    var new_message = [];
+    var msg = {
+      _id: id_msg,
+      text: result_serv.data.output.generic[0].text,
+      createdAt: new Date(),
+      user: {
+        _id: 'watson_id',
+        name: 'Watson',
+        avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/00/IBM_Watson_Logo_2017.png/220px-IBM_Watson_Logo_2017.png',
+      }
+    }
+    new_message.push(msg);
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, new_message),
+      context: result_serv.data.context,
+      id_message: id_msg,
+      is_waiting: false
     }))
   }
 
@@ -62,10 +114,19 @@ export default class ChatScreen extends React.Component {
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
           user={{
-            _id: 'Maxime',
+            _id: 'maxime_id',
             avatar: 'https://cdn-images-1.medium.com/max/1200/1*paQ7E6f2VyTKXHpR-aViFg.png',
             name: 'Maxime'
           }}
+          renderFooter={() => (
+            <View>
+              {
+                this.state.is_waiting ?
+                  <ActivityIndicator/>
+                : null
+              }
+            </View>
+          )}
         />
       </Container>
     )
