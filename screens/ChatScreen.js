a56f302a9f95799026f7567b374a9b1c88f4bbea
@maxgfr <maxgolfier@gmail.com>
2019-06-20 16:54:46 +0200

    push

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -22,8 +22,14 @@ export default class ChatScreen extends React.Component {
     messages: [],
     session_id: '',
     context: {},
-    id_message: 1,
-    is_waiting: true
+    id_message: 0,
+    is_waiting: false,
+    watson_id: 'watson_id',
+    watson_name: 'Watson',
+    watson_pp: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/00/IBM_Watson_Logo_2017.png/220px-IBM_Watson_Logo_2017.png',
+    user_id: 'user_id',
+    user_pp: 'https://cdn-images-1.medium.com/max/1200/1*paQ7E6f2VyTKXHpR-aViFg.png',
+    user_name: 'Maxime'
   }
 
   static navigationOptions = {
@@ -31,41 +37,34 @@ export default class ChatScreen extends React.Component {
   };
 
   async componentWillMount() {
-    const response_serv = await fetch('https://insurassistant-anxious-hyena.eu-gb.mybluemix.net/create-session', {
+    this.setState(previousState => ({
+      is_waiting: true
+    }));
+    const response_serv_init = await fetch('https://insurassistant-anxious-hyena.eu-gb.mybluemix.net/create-session', {
         method: 'GET'
     });
-    var result_serv = await response_serv.json();
-    console.log(result_serv);
-    this.setState({
-      messages: [
-        {
-          _id: this.state.id_message,
-          text: 'Hi ! How can I help you ?',
-          createdAt: new Date(),
-          user: {
-            _id: 'watson_id',
-            name: 'Watson',
-            avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/00/IBM_Watson_Logo_2017.png/220px-IBM_Watson_Logo_2017.png',
-          },
-        },
-      ],
-      session_id: result_serv.data.session_id,
-      is_waiting: false
-    })
+    var result_serv_init = await response_serv_init.json();
+    //console.log(result_serv_init);
+    this.state.session_id = result_serv_init.data.session_id;
+    this.sendMessage('Bonjour');
   }
 
-  async onSend(messages = []) {
+  onSend(messages = []) {
     var message = messages[0].text;
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
       is_waiting: true
     }))
+    this.sendMessage(message);
+  }
+
+  sendMessage = async (message) => {
     var data = {
     	message: message,
     	context: this.state.context,
     	session_id: this.state.session_id
     }
-    console.log(data)
+    //console.log(data)
     const response_serv = await fetch('https://insurassistant-anxious-hyena.eu-gb.mybluemix.net/send-message', {
         method: 'POST',
         headers: {
@@ -74,17 +73,18 @@ export default class ChatScreen extends React.Component {
         body: JSON.stringify(data)
     });
     var result_serv = await response_serv.json();
-    console.log(result_serv)
+    //console.log(result_serv)
     var id_msg = this.state.id_message + 1;
+    var txt = result_serv.data.output.generic[0].text
     var new_message = [];
     var msg = {
       _id: id_msg,
-      text: result_serv.data.output.generic[0].text,
+      text: txt,
       createdAt: new Date(),
       user: {
-        _id: 'watson_id',
-        name: 'Watson',
-        avatar: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/00/IBM_Watson_Logo_2017.png/220px-IBM_Watson_Logo_2017.png',
+        _id: this.state.watson_id,
+        name: this.state.watson_name,
+        avatar: this.state.watson_pp
       }
     }
     new_message.push(msg);
@@ -114,9 +114,9 @@ export default class ChatScreen extends React.Component {
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
           user={{
-            _id: 'maxime_id',
-            avatar: 'https://cdn-images-1.medium.com/max/1200/1*paQ7E6f2VyTKXHpR-aViFg.png',
-            name: 'Maxime'
+            _id: this.state.user_id,
+            avatar: this.state.user_pp,
+            name: this.state.user_name
           }}
           renderFooter={() => (
             <View>
